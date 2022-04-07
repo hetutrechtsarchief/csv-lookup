b98247a53d62bc95c821c6ae9a929175d0687992
Rick Companje <rick@companje.nl>
2022-04-07 11:22:13 +0200

    Update csv-lookup.py

--- csv-lookup.py
+++ csv-lookup.py
@@ -22,40 +22,7 @@ writer.fieldnames.append(LOOKUP_VALUE_COL)
 writer.writeheader()
 
 for r in reader:
-  input_value = r[INPUT_KEY_COL] + ".jpg" # .jpg is tijdelijk om te voorkomen dat ik extensies moet weghalen uit de csv
-  
-  # if input_value in lut:
+  input_value = r[INPUT_KEY_COL] # + ".jpg" # .jpg toevoegen hier was tijdelijk om te voorkomen dat ik extensies moet weghalen uit de csv
   output_value = lut[input_value][LOOKUP_VALUE_COL]
-  # print(output_value)
   r[LOOKUP_VALUE_COL] = output_value 
   writer.writerow(r)
-
-
-
-
-
-#   print(r["BESCHRIJVING"])
-
-
-# image_name=BESCHRIJVING
-
-
-# split_field_name = argv[1]
-# input_filename = argv[2]
-# output_folder = argv[3] 
-# output_writers = {}
-
-# with open(input_filename, 'rt', encoding=encoding) as input_file:
-#   reader = csv.reader(input_file, delimiter=delimiter)
-#   header = next(reader)
-
-#   for r in csv.DictReader(input_file, fieldnames=header, delimiter=delimiter):
-
-#     split_field_value = dict(r)[split_field_name]
-
-#     if split_field_value not in output_writers:
-#       output_filepath = output_folder + "/" + os.path.basename(input_filename).replace(".csv",f"-{split_field_value}.csv")
-#       output_writers[split_field_value] = csv.DictWriter(open(output_filepath,"w"), fieldnames=header, delimiter=delimiter)
-#       output_writers[split_field_value].writeheader()
-
-#     output_writers[split_field_value].writerow(r)
